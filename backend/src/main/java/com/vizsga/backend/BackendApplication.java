260881b7e71bd109889ee8e70933edfbb8ab271d
Gerecs Diána <gerecsdiana@gmail.com>
2024-05-21 06:58:49 +0200

    Initial commit

---
+++ backend/src/main/java/com/vizsga/backend/BackendApplication.java
@@ -0,0 +1,24 @@
+/*
+ * File: vizsga
+ * Author: Gerecs Diána
+ * Copyright: 2024, Gerecs Diána
+ * Group: Szoft II/1/E
+ * Date: 2024-05-21
+ * Github: https://github.com/GerecsDia/
+ * Licenc: GNU GPL
+ */
+
+
+package com.vizsga.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
